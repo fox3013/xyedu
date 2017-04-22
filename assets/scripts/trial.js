377fdaaad783296d6f3ab3e38fead9cc5feaaf26
Fox Zhou <yuan.zhou@eumlab.com>
2017-04-22 23:41:29 +0800

    动态判断video的css

--- assets/scripts/trial.js
+++ assets/scripts/trial.js
@@ -9,4 +9,53 @@ $(document).ready(function(){
     $('#video-cover').attr('poster', image);
     $('#video-cover').attr('src', src);
   });
+
+
+
+
+
+  var mb = myBrowser();
+  if ("IE" == mb) {
+
+  }
+  if ("FF" == mb) {
+
+  }
+  var isLoaded = false;
+
+  if ("Chrome" == mb) {
+    if (!isLoaded) {
+      $('head').append("<link rel='stylesheet' href='./assets/style/video.css' media='screen'>");
+    }
+    isLoaded = true;
+
+  }
+  if ("Opera" == mb) {
+
+  }
+  if ("Safari" == mb) {
+
+  }
+
+
+  function myBrowser(){
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1;
+    if (isOpera) {
+        return "Opera"
+    }; //判断是否Opera浏览器
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "FF";
+    } //判断是否Firefox浏览器
+    if (userAgent.indexOf("Chrome") > -1){
+      return "Chrome";
+    }
+    if (userAgent.indexOf("Safari") > -1) {
+        return "Safari";
+    } //判断是否Safari浏览器
+    if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+        return "IE";
+    }; //判断是否IE浏览器
+  }
+
 });
