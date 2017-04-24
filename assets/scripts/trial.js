e84a1c0014b0d08c67e1943baf9080aac03402ad
Fox Zhou <yuan.zhou@eumlab.com>
2017-04-24 09:47:08 +0800

    微调

--- assets/scripts/trial.js
+++ assets/scripts/trial.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
   $('a.video-button').on('click touch', function(e){
-    $('.active').removeClass('active');
+    $('.video-wrapper .active').removeClass('active');
     $(this).parent().addClass('active');
     image = "./assets/images/img-" + $(this).data('id') +'.png';
     src = "http://ressync.xy.jxcstatic.com/" + $(this).data('src')
